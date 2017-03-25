17c28dc4168796ab921d0079674ab03a4f174bb6
tonyqiu1019 <tq7bw@virginia.edu>
2017-03-25 15:06:11 -0400

    fix stupid bugs

--- models/api/models.py
+++ models/api/models.py
@@ -4,11 +4,11 @@ import datetime
 class Article(models.Model):
     title = models.CharField(max_length=1024, default='')
     author = models.CharField(max_length=1024, default='')
-    date = models.DateField(_('Date'), default=datetime.date.today)
+    date = models.DateField(default=datetime.date.today)
 
     def __str__(self): return 'Article: ' + title
 
-    class Meta: ordering = (date, title, )
+    class Meta: ordering = ('date', 'title', )
 
 
 class Sentence(models.Model):
--- models/models/urls.py
+++ models/models/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(api.urls)),
+    url(r'^api/', include('api.urls')),
 ]
